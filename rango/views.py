b827774de554d325ab36a94c1f754f7819cd00e4
Mike Harling <mike.harling@rocketmail.com>
2015-02-08 14:35:54 +0000

    Chapter 10 - completed

--- rango/views.py
+++ rango/views.py
@@ -213,7 +213,8 @@ def user_login(request):
 # Restricted views
 @login_required
 def restricted(request):
-    return HttpResponse("Since you are logged in, you can see this text!")
+   # return HttpResponse("Since you are logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
 
 # Use the login_required() decorator to ensure only those logged in can access the view.
 @login_required
